db111bd73ad1e4587270ffba2a1ea5676002ab3d
David P. Adams <dpadams@fullerton.edu>
2023-09-20 11:38:42 -0700

    update with mainweb styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,49 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const themeButtons = document.querySelectorAll('[data-bs-theme-value]');
-    const body = document.body;
-    const savedTheme = localStorage.getItem('theme');
+$(document).ready(function() {
+    // Check for saved theme preference or default to 'auto'
+    let themePreference = localStorage.getItem('themePreference') || 'auto';
+    setTheme(themePreference);
 
-    function updateThemeIcon(theme) {
-        const themeToggle = document.getElementById('bd-theme');
-        const icon = themeToggle.querySelector('.theme-icon-active');
+    // Update the dropdown to reflect the saved theme preference
+    updateDropdown(themePreference);
+
+    // Event listener for theme change
+    $('[data-bs-theme-value]').on('click', function() {
+        let selectedTheme = $(this).attr('data-bs-theme-value');
+        setTheme(selectedTheme);
+        updateDropdown(selectedTheme);
+    });
+
+    function setTheme(theme) {
+        // Remove all possible theme classes first
+        $('body').removeClass('light-theme dark-theme');
 
         switch (theme) {
             case 'light':
-                icon.innerHTML = '<use href="#sun-fill"></use>';
+                $('body').addClass('light-theme');
                 break;
             case 'dark':
-                icon.innerHTML = '<use href="#moon-stars-fill"></use>';
+                $('body').addClass('dark-theme');
                 break;
             default:
-                icon.innerHTML = '<use href="#circle-half"></use>';
+                // Auto theme based on user's system preference
+                if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                    $('body').addClass('dark-theme');
+                } else {
+                    $('body').addClass('light-theme');
+                }
                 break;
         }
+        // Save the theme preference
+        localStorage.setItem('themePreference', theme);
     }
 
-    if (savedTheme) {
-        body.setAttribute('data-theme', savedTheme);
-        updateThemeIcon(savedTheme);
-    } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        body.setAttribute('data-theme', 'dark');
-        updateThemeIcon('dark');
+    function updateDropdown(theme) {
+        // Get the SVG of the selected theme
+        const selectedSvg = $(`[data-bs-theme-value=${theme}] svg`).clone();
+    
+        // Clear the current SVG inside the main button and append the new one
+        $('#bd-theme svg').remove();
+        $('#bd-theme').prepend(selectedSvg);
     }
-
-    themeButtons.forEach(button => {
-        button.addEventListener('click', function(e) {
-            const selectedTheme = this.getAttribute('data-bs-theme-value');
-            body.setAttribute('data-theme', selectedTheme);
-            localStorage.setItem('theme', selectedTheme);
-            updateThemeIcon(selectedTheme);
-        });
-    });
+    
 });
